2e36054e128f32e47c154f78ceecfc2a4c9579d9
mTangtrongwanit <MTangtrongwanit@esri.com>
2024-09-11 16:50:49 -0400

    Implementing i18next library for translation.

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,11 @@ import { AppContextProvider } from "./contexts/AppContext";
 
 // #region ------------------------ Resources ----------------------------------
 import "@/styles/index.css";
+
+// import where needed to bring in i18n translation
+import './utils/i18n';
+import { useTranslation, Trans } from 'react-i18next';
+
 // #endregion --------------------- Resources ----------------------------------
 // #endregion ====================== IMPORTS ===================================
 // #region ======================== CONSTANTS ==================================
@@ -37,6 +42,19 @@ const router = createBrowserRouter(
 // #endregion ===================== CONSTANTS ==================================
 // #region =================== EXPORTED COMPONENT ==============================
 function App() {
+
+  // define and use the 't' function for translating content.
+  // 'i18n' to get the i18n instance in order to change the language
+  const { t, i18n } = useTranslation();
+
+  // Can change language here
+  // TODO: will need to tie this to a dropdown selector
+  // const lng = 'es';
+  // TODO: Need to tie this to a button, etc.  
+  // i18n.changeLanguage(lng);
+
+  const count = 3;
+
   // #region ------------------ Hooks (Resources) ------------------------------
   // #endregion --------------- Hooks (Resources) ------------------------------
   // #region -------------------- Hooks (State) --------------------------------
@@ -53,6 +71,12 @@ function App() {
   // #region ----------------------- Render ------------------------------------
   return (
     <AppContextProvider>
+      <p>{t('title', { name: 'John' })}</p>
+          <p>{t('description.part1')}</p>
+          <p>{t('description.part2')}</p>
+          <Trans i18nKey="userMessagesUnread" count={count}>
+            You have {{ count }} unread message.
+          </Trans>
       <RouterProvider router={router} />
     </AppContextProvider>
   );
