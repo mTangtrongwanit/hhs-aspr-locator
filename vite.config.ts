0469b7ba5edac8189bcdaa3ae0b49ac762bda8af
Lillie Bahrami <l+git@sparkllie.com>
2024-09-19 11:07:45 -0600

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       include: "**/*.svg",
     }),
   ],
-  base: `/${asprPackage.homepage}/`,
+  // base: `/${asprPackage.homepage}/`,
   resolve: {
     alias: {
       // Resolve the ~ alias to the src/ directory
