4b282aac8ef17ed97c0154c266962443e5431325
Shawn Goulet <sgoulet@esri.com>
2024-09-24 10:58:19 -0400

    Add base to vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       include: "**/*.svg",
     }),
   ],
-  // base: `/${asprPackage.homepage}/`,
+  base: process.env.BASE_PATH ? `/${process.env.BASE_PATH}/ ` : '/',
   resolve: {
     alias: {
       // Resolve the ~ alias to the src/ directory
