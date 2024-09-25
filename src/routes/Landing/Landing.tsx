3bdcd3c75a82d86829b815b619afc7ea14464496
Lillie Bahrami <l+git@sparkllie.com>
2024-09-25 16:24:48 -0600

    feat: accordion placeholder

--- src/routes/Landing/Landing.tsx
+++ src/routes/Landing/Landing.tsx
@@ -17,7 +17,8 @@ import {
   StyledSearchContainer,
   StyledContextRow,
 } from "./Landing.styles";
-// #endregion ----------- Custom Components / Utilities ------------------------
+import * as Accordion from '@radix-ui/react-accordion';
+import { ChevronDownIcon } from '@radix-ui/react-icons';// #endregion ----------- Custom Components / Utilities ------------------------
 
 // #region ------------------------ Resources ----------------------------------
 // import { type Props } from "./Landing.types";
@@ -76,10 +77,43 @@ const Landing = () => {
         <h2 className="visually-hidden">
           {t("Landing.Context Container Screenreader Heading")}
         </h2>
-        {/* <p>&str Locations participating in the U.S. Government&nbsp;
-        <a href="/todo">Patient Assistance Program</a>       ...
-      </p> */}
-        <div className="dev-placeholder">Accordion placeholder</div>
+        <p>Locations participating in the U.S. Government&nbsp;
+          <a>Patient Assistance Program</a> operated by Pfizer offering free Paxlovid for eligible patients are included on this website. Eligible patients with a Paxlovid prescription can enroll in the program at <a>https://paxlovid.iassist.com.</a> Other <a>assistance programs</a> are available for Paxlovid, Lagevrio, and Veklury.
+        </p>
+        <Accordion.Root className="AccordionRoot" type="single" defaultValue="item-1" collapsible>
+    <Accordion.Item className="AccordionItem" value="item-1">
+    <Accordion.Header className="AccordionHeader">
+    <Accordion.Trigger
+      className={'AccordionTrigger'}
+    >
+      Frequently Asked Questions
+      <ChevronDownIcon className="AccordionChevron" aria-hidden />
+    </Accordion.Trigger>
+  </Accordion.Header>      <Accordion.Content>TODO</Accordion.Content>
+    </Accordion.Item>
+
+    <Accordion.Item className="AccordionItem" value="item-2">
+    <Accordion.Header className="AccordionHeader">
+    <Accordion.Trigger
+      className={'AccordionTrigger'}
+    >
+      Disclaimer
+      <ChevronDownIcon className="AccordionChevron" aria-hidden />
+    </Accordion.Trigger>
+  </Accordion.Header>
+      <Accordion.Content>
+      <div className="AccordionContentText">
+      <p>
+        Additional information on COVID-19 treatments can be found on the <a></a>ASPR COVID-19 Treatments page.  For questions regarding this site, contact 1-800-232-0233 (TTY  888-720-7489). To learn more about COVID-19 testing, including how to  order free test kits, visit COVID.gov.
+
+        </p>
+      * This content is not fully Section 508 conformant. If you need assistance, please contact gis@hhs.gov. Tabular access to the data is also available.     
+
+      </div>
+      </Accordion.Content>
+    </Accordion.Item>
+  </Accordion.Root>
+
       </StyledContextRow>
     </StyledLandingContent>
   );
