943e926c5a29141de2875eed3145b43674e5d8fd
Shawn Goulet <sgoulet@esri.com>
2024-10-04 15:07:23 -0400

    Fix temporary lint ignore.

--- src/components/PopoverMultiSelect/PopoverMultiSelect.tsx
+++ src/components/PopoverMultiSelect/PopoverMultiSelect.tsx
@@ -56,7 +56,7 @@ const PopoverMultiSelect = ({ type }: PopoverMultiSelectProps) => {
   ]);
   const [selectedFilters, setSelectedFilters] = useState<string[]>([]);
   // TODO: This will come from the single select comonent based on the selected illness
-  // tslint:disable-next-line: no-unused-variable
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [selectedIllness, setSelectedIllness] = useState<string>("Flu");
   const [treatments, setTreatments] = useState<string[]>([]);
   // #endregion ----------------- Hooks (State) --------------------------------
