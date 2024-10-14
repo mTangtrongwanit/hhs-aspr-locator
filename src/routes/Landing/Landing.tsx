a3562a69aec60bfdada181c2131e11f88631bd59
Lillie Bahrami <l+git@sparkllie.com>
2024-10-14 12:56:19 -0600

    feat: jsx structure for first three FAQs

--- src/routes/Landing/Landing.tsx
+++ src/routes/Landing/Landing.tsx
@@ -80,10 +80,11 @@ const Landing = () => {
           </a>
         </p>
         <p>
-          This website includes locations participating in the U.S. Government&nbsp;
-          <a href="https://paxlovid.iassist.com/">Patient Assistance Program</a>&nbsp;
-          operated by Pfizer, offering free Paxlovid for eligible patients.
-          Other assistance programs are available for&nbsp;
+          This website includes locations participating in the U.S.
+          Government&nbsp;
+          <a href="https://paxlovid.iassist.com/">Patient Assistance Program</a>
+          &nbsp; operated by Pfizer, offering free Paxlovid for eligible
+          patients. Other assistance programs are available for&nbsp;
           <a href="https://aspr.hhs.gov/COVID-19/Treatments/Pages/Possible-Treatment-Options-for-COVID19.aspx#oral-antivirals">
             COVID-19
           </a>{" "}
@@ -131,28 +132,390 @@ const Landing = () => {
           defaultValue="item-1"
           collapsible
         >
+          {
+            //#region Drawer 1
+          }
           <Accordion.Item className="AccordionItem" value="item-1">
             <Accordion.Header className="AccordionHeader">
-              {
-                //#region Accordion Drawer 1
-              }
               <Accordion.Trigger className={"AccordionTrigger"}>
-                Frequently Asked Questions
+                Which outpatient medications available to treat COVID-19 and flu
+                appear on this locator?
                 <ChevronDownIcon className="AccordionChevron" aria-hidden />
               </Accordion.Trigger>
             </Accordion.Header>
             <Accordion.Content className="AccordionContent">
               <div className="AccordionContentText">
-                <StyledFAQTitle>Placeholder - FAQ 1</StyledFAQTitle>
-                <p>Placeholder Answer - FAQ 1</p>
+                <p>
+                  The COVID-19 and flu treatment locator can be used to help you
+                  find FDA approved or authorized treatments for these diseases.{" "}
+                </p>
+                <StyledFAQTitle>COVID-19</StyledFAQTitle>
+                <ul>
+                  <li>
+                    Three outpatient treatments are currently authorized or
+                    approved by the U.S. Food and Drug Administration (FDA) to
+                    help reduce the risk of hospitalization or death for
+                    patients with mild to moderate COVID-19:
+                    <ul>
+                      <li>Oral Paxlovid (nirmatrelvir with ritonavir)</li>
+                      <li>Oral Lagevrio (molnupiravir)</li>
+                      <li>Intravenous outpatient Veklury (remdesivir)</li>
+                    </ul>
+                  </li>
+                  <li>
+                    Visit{" "}
+                    <a href="https://aspr.hhs.gov/COVID-19/Treatments/Pages/Possible-Treatment-Options-for-COVID19.aspx">
+                      Treatment Options for COVID-19
+                    </a>{" "}
+                    to learn more about these medications.
+                  </li>
+                  <li>
+                    All COVID treatments listed on this site require a
+                    prescription from a health care provider and are most
+                    effective if you take them as soon as you get sick, and
+                    ideally within 5 days of developing symptoms.
+                  </li>
+                </ul>
+
+                <StyledFAQTitle>Influenza (Flu)</StyledFAQTitle>
+                <ul>
+                  <li>
+                    The following outpatient treatments are currently approved
+                    by FDA and recommended by CDC to treat flu:
+                    <ul>
+                      <li>
+                        Oral oseltamivir (available as a generic version or
+                        under the trade name Tamiflu),
+                      </li>
+                      <li>Oral baloxavir (Xofluza) </li>
+                      <li>Inhaled zanamivir (Relenza) </li>
+                      <li>Intravenous peramivir (Rapivab)</li>
+                    </ul>
+                  </li>
+                  <li>
+                    Please visit{" "}
+                    <a href="https://www.cdc.gov/flu/treatment/whatyoushould.htm">
+                      What are Flu Antiviral Drugs | CDC
+                    </a>{" "}
+                    to learn more about treatments for flu. Healthcare providers
+                    can visit CDC’s{" "}
+                    <a href="https://www.cdc.gov/flu/professionals/antivirals/summary-clinicians.htm">
+                      Influenza Antiviral Medications: Summary for Clinicians.{" "}
+                    </a>
+                  </li>
+                  <li>
+                    All flu treatments listed on this site require a
+                    prescription from a health care provider and are most
+                    effective if you take them as soon as you get sick, and
+                    ideally within 48 hours of developing symptoms.
+                  </li>
+                </ul>
               </div>
             </Accordion.Content>
           </Accordion.Item>
           {
-            //#endregion Accordion Drawer 1
+            //#endregion Drawer 1
           }
           {
-            //#region Accordion Drawer 2
+            //#region Drawer 2
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText">
+                <ul>
+                  <li>
+                    Antiviral drugs are prescription medicines that fight
+                    against the specific virus they are prescribed for –
+                    COVID-19 antivirals work to treat COVID-19, and flu
+                    antivirals work to treat influenza. Antiviral drugs are
+                    different from antibiotics, which fight against bacterial
+                    infections, and come in different forms:
+                    <ul>
+                      <li>
+                        Oral antiviral drugs, are taken by mouth (in a pill or
+                        liquid suspension form)
+                      </li>
+                      <li>
+                        Inhaled antiviral drugs are breathed in by mouth as
+                        directed (e.g., can be administered by an inhaler).
+                      </li>
+                      <li>
+                        Intravenous antiviral drugs are given directly into a
+                        vein with a needle or a tube.
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 2
+          }
+          {
+            //#region Drawer 3
+          }
+          <Accordion.Item className="AccordionItem" value="item-3">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                Who can take the outpatient COVID-19 medications listed on this
+                website?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText">
+                <ul>
+                  <li>
+                    Paxlovid: Adults and children (12 years of age and older,
+                    weighing at least 88 pounds [40 kg]), who are at high risk
+                    for getting very sick from COVID-19 and who have mild to
+                    moderate symptoms.
+                  </li>
+                  <li>
+                    Lagevrio: Adults 18 years and older, who are at high risk
+                    for getting very sick from COVID-19, have mild to moderate
+                    symptoms, and do not have access to other COVID-19
+                    outpatient treatment options or other treatment options are
+                    not appropriate for them.
+                  </li>
+                  <li>
+                    Veklury: In the outpatient setting, adults and children (28{" "}
+                    <strong>days</strong> of age or older and weighing at least
+                    7 pounds [3 kg]), who have mild to moderate symptoms of
+                    COVID-19 and are at high risk for getting very sick from
+                    COVID-19.
+                    <ul>
+                      <li>
+                        This medication is given through an intravenous infusion
+                        and may be given in the outpatient (not staying in the
+                        hospital) setting over three consecutive visits or in
+                        the inpatient (staying in the hospital) setting. This
+                        website lists only outpatient locations where Veklury is
+                        available.{" "}
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    Paxlovid and Lagevrio must be taken within 5 days of symptom
+                    onset to be effective. Veklury outpatient administration is
+                    within 7 days of symptom onset.
+                  </li>
+                  <li>
+                    To learn more about indications for these products, see{" "}
+                    <a href="https://aspr.hhs.gov/COVID-19/Treatments/Pages/Possible-Treatment-Options-for-COVID19.aspx">
+                      Treatment Options for COVID-19.
+                    </a>{" "}
+                    Healthcare providers can refer to the{" "}
+                    <a href="https://aspr.hhs.gov/COVID-19-Therapeutics/Side-by-Side-Overview/Pages/default.aspx">
+                      Side-by-Side Overview of Therapeutics Authorized or
+                      Approved for the Treatment of Mild to Moderate COVID-19.
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 3
+          }
+          {
+            //#region Drawer 4
+          }
+          <Accordion.Item className="AccordionItem" value="item-4">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                Who can take the outpatient flu medications listed on this
+                website?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 4
+          }
+          {
+            //#region Drawer 5
+          }
+          <Accordion.Item className="AccordionItem" value="item-5">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                Can children take the antiviral medications listed on this
+                website?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 5
+          }
+          {
+            //#region Drawer 6
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                Can pregnant people take flu and COVID-19 antiviral medications?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 6
+          }
+          {
+            //#region Drawer 7
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 7
+          }
+          {
+            //#region Drawer 8
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 8
+          }
+          {
+            //#region Drawer 9
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 9
+          }
+          {
+            //#region Drawer 10
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 10
+          }
+          {
+            //#region Drawer 11
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 11
+          }
+          {
+            //#region Drawer 12
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 12
+          }
+          {
+            //#region Drawer 13
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 13
+          }
+          {
+            //#region Drawer 14
+          }
+          <Accordion.Item className="AccordionItem" value="item-2">
+            <Accordion.Header className="AccordionHeader">
+              <Accordion.Trigger className={"AccordionTrigger"}>
+                2. What are antivirals?
+                <ChevronDownIcon className="AccordionChevron" aria-hidden />
+              </Accordion.Trigger>
+            </Accordion.Header>
+            <Accordion.Content className="AccordionContent">
+              <div className="AccordionContentText"></div>
+            </Accordion.Content>
+          </Accordion.Item>
+          {
+            //#endregion Drawer 14
+          }
+          {
+            //#region Disclaimer
           }
           <Accordion.Item className="AccordionItem" value="item-2">
             <Accordion.Header className="AccordionHeader">
@@ -182,9 +545,6 @@ const Landing = () => {
               </div>
             </Accordion.Content>
           </Accordion.Item>
-          {
-            //#endregion Accordion Drawer 2
-          }
         </Accordion.Root>
         {
           //#endregion Accordion
