97ae40cd84c81521ec688a50dd82a7700b42cd05
Lillie Bahrami <l+git@sparkllie.com>
2024-10-15 15:16:32 -0600

    fix: order languages alphabetically

--- src/config/config.ts
+++ src/config/config.ts
@@ -70,16 +70,16 @@ const config: StaticConfiguration = {
   options: {
     languageOptions: [
       {
+        label: "Chinese (Simplified)",
+        value: "zh",
+      },
+      {
         label: "English",
         value: "en",
       },
       {
         label: "Spanish",
         value: "es",
-      },
-      {
-        label: "Chinese (Simplified)",
-        value: "zh",
       },
     ],
     sortOptions: [
