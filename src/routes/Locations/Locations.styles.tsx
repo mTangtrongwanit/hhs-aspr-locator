dd94b01d70a82195c954e030332ad9b3e2ad7eda
Chandan Mahapatra <chandan.m1992@gmail.com>
2024-10-15 15:59:45 -0700

    Updated css style for the button and icons style fix

--- src/routes/Locations/Locations.styles.tsx
+++ src/routes/Locations/Locations.styles.tsx
@@ -55,27 +55,28 @@ export const StyledSearchContainer = styled.section`
   // --- Children ---
   #listViewToggle {
     display: none;
-    height: var(--search-container-item-height);
-    // --- Sizing / Box-Model ---
-    width: min-content;
-    padding: calc(var(--unit) / 2) calc(var(--unit)); //vert horz
-
+    border: var(--border) solid var(--brand);
     align-items: center;
-    gap: calc(var(--unit) / 2);
-    // --- Decorative ---
-    white-space: nowrap;
-    background: #fff;
+    padding: calc(var(--unit) * 0.75) calc(var(--unit) * 0.75);
+    gap: calc(var(--unit) * 0.25);
+    font-size: var(--text-0);
+    font-weight: 600 !important;
+    line-height: 125%;
     color: var(--brand);
     border-radius: var(--radius);
-    border: 1px solid var(--brand);
-    font-weight: bold;
-    font-size: var(--text-0);
+    background: white;
+    text-decoration: none;
     // --- States ---
 
     &:hover,
     &:focus {
-      border-color: var(--accent);
+      color: white;
+      background: var(--accent);
       cursor: pointer;
+    }
+
+    &:hover > svg > g {
+      fill: white;
     }
 
     @media ${Breakpoints.sm} {
