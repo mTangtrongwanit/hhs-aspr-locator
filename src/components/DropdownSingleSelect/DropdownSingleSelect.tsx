3dec785737aed0725d7f8d2e99ab54ae11735615
Lillie Bahrami <l+git@sparkllie.com>
2024-10-18 12:54:42 -0600

    fix: rm title from DSS

--- src/components/DropdownSingleSelect/DropdownSingleSelect.tsx
+++ src/components/DropdownSingleSelect/DropdownSingleSelect.tsx
@@ -144,11 +144,6 @@ const DropdownSingleSelect = ({
               ? "hhs-outline-button hhs-outline-button-landing"
               : "hhs-primary-button"
           }
-          title={
-            placeholder && selectedOption.value === ""
-            ? placeholder
-            : selectedLabel
-          }
           aria-label={placeholder && selectedOption.value === ""
             ? placeholder
             : selectedLabel}
