4aa6a3f5fc8b0fbc1d09ea0aa7644be3547e3588
Lillie Bahrami <l+git@sparkllie.com>
2024-10-18 13:08:48 -0600

    fix: add question mark

--- src/components/PopoverMultiSelect/PopoverMultiSelect.tsx
+++ src/components/PopoverMultiSelect/PopoverMultiSelect.tsx
@@ -401,7 +401,7 @@ const PopoverMultiSelect = ({ type }: PopoverMultiSelectProps) => {
     <StyledPopoverMultiSelect>
       <PopoverMenu.Root>
         <PopoverMenu.Trigger className="hhs-primary-button">
-          {type === "medications" ? "Looking for Specific Medications" : "Filters"}{" "}
+          {type === "medications" ? "Looking for Specific Medications?" : "Filters"}{" "}
           <ChevronDownIcon />
           <span className="PopOverFilterCount">
             &#40;
