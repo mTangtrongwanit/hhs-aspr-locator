35df47c3c9d1193a3b455f9cac5252edf4b2dd49
mTangtrongwanit <MTangtrongwanit@esri.com>
2024-11-20 18:24:35 -0500

    Add conditional for html element to show version if mode is 'development'

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -207,6 +207,9 @@ const Footer = () => {
               <br />
               200 Independence Ave., Washington, DC 20201
             </StyledFooterAddress>
+            
+            {APP_MODE === 'development' && <StyledFooterAddress>Development Version: {APP_VERSION}</StyledFooterAddress>}
+            
           </StyledFooterAddressContainer>
         </StyledFooterFirstColumn>
         <StyledFooterSecondColumn>
@@ -309,6 +312,7 @@ const Footer = () => {
             </StyledFooterMediaLink>
           </StyledFooterMediaLinksContainer>
         </StyledFooterSecondColumn>
+        
       </StyledFooter>
     </StyledFooterContainer>
   );
