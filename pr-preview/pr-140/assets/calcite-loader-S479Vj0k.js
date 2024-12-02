d5760821985e65c52c3607dc8692e375ae710056
mTangtrongwanit <mTangtrongwanit@users.noreply.github.com>
2024-12-02 21:57:08 +0000

    Deploy preview for PR 140 🛫

---
+++ pr-preview/pr-140/assets/calcite-loader-S479Vj0k.js
@@ -0,0 +1,5 @@
+import{L as o,d as t}from"./loader-B686xhFx.js";import"./index-CHJoLRY3.js";import"./dom-BYlR_Vu-.js";import"./locale-CFTb5TRv.js";import"./observers-BV9PPNCx.js";/*!
+ * All material copyright ESRI, All Rights Reserved, unless otherwise specified.
+ * See https://github.com/Esri/calcite-design-system/blob/dev/LICENSE.md for details.
+ * v2.12.0
+ */const p=o,s=t;export{p as CalciteLoader,s as defineCustomElement};
