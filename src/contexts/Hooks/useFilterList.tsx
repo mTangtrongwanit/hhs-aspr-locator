41ff91b4d115b80f8a1c0bf063ac8a18382b58a3
mTangtrongwanit <MTangtrongwanit@esri.com>
2025-02-19 13:02:01 -0500

    Adjust the circle sizing by adjusting search radius params.

--- src/contexts/Hooks/useFilterList.tsx
+++ src/contexts/Hooks/useFilterList.tsx
@@ -98,7 +98,7 @@ export const useFilterList = ({
     // find the appropriate search radius
     const MAX_SEARCH_RADIUS = 50; // starting full search radius
     const MIN_SEARCH_RADIUS_INCREMENT = 1; // minimum increment to reduce the search radius
-    const DISPLAY_COUNT = 100; // number of results to display
+    const DISPLAY_COUNT = 300; // number of results to display
     const COUNT_BUFFER_FACTOR = 10; // conservative buffer to ensure we don't miss any points
     const DENSITY_ADJUSTMENT_FACTOR = 0.75; // density adjustment factor to account for clustering of points near cities (also assuming the search point is near a city, otherwise why would we have too many results?)
     const narrowSearchRadius = (radius: number): Promise<number> =>
