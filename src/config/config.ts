d4ad833ee78777ba1b4f7ee8a07e49e7855689bd
mTangtrongwanit <MTangtrongwanit@esri.com>
2025-02-25 15:20:39 -0500

    Move locale json files to public folder

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,8 @@
 // #region ------------------------ Resources ----------------------------------
 import { type StaticConfiguration } from "./config.types";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import * as en from "../utils/locales/en.json";
+// import * as en from "../utils/locales/en.json";
+import * as en from "../../public/locales/en.json";
 // #endregion ------------------------- Resources ---------------------------------
 // #endregion ========================== IMPORTS ==================================
 
